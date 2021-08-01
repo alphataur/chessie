e8ec97650f0262a0e6cdbcdc55af14f8656de695
Nikhil Surya Mukhi <darksublimethoughts@gmail.com>
2021-08-02 03:39:20 +0530

    added cost function

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,28 @@
+const pieceScores = {
+  "R": 5.63,
+  "N": 3.05,
+  "B": 3.33,
+  "Q": 9.5,
+  "K": 1000,
+}
+
+function pieceVal(piece){
+  switch(piece.toLowerCase()){
+    case "r":
+      return 5.63
+    case "n":
+      return 3.05
+    case "b":
+      return 3.33
+    case "q":
+      return 9.5
+    case "k":
+      return 1000
+    default:
+      return 1
+  }
+}
+
 class Game{
     constructor(repr){
         this.repr = this.initBoard(repr)
@@ -23,6 +48,20 @@ class Game{
     getMoves(){
         return this.board.moves()
     }
+    evalScore(){
+      //this cost functions returns score in white's perspective i.e higher the score the more possibility of white winning
+      let pieces = this.board.fen().split(" ")[0].split('').filter((c)=>{
+        return /^[A-Z]$/i.test(c)
+      })
+      return pieces.reduce((a, e) => {
+        if(/^[A-Z]$/.test(e)){
+          return a + pieceVal(e)
+        }
+        else{
+          return a - pieceVal(e)
+        }
+      }, 0)
+    }
     autoMoves(){
         this.timer = setInterval(()=>{
             if(this.isOver())
@@ -31,12 +70,13 @@ class Game{
             let moves = this.getMoves()
             let randomMove = moves[Math.floor(Math.random()*moves.length)]
             console.log(randomMove)
+            console.log(this.evalScore())
             this.makeMove(randomMove)
-        },100)
+        },1000)
     }
 }
 
 window.onload = function(){
-    let game = new Game()
-    game.autoMoves()
+  let game = new Game()
+  console.log(Math.round(game.evalScore(), 2))
 }
